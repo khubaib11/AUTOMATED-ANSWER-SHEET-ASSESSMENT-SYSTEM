e939dc6f6bf8bb088f0e642017a7d83ee30c5703
khubaib khan <khankhubaib089@gmail.com>
2024-11-26 02:50:08 +0500

    install flowbite and create routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
-
+import { BrowserRouter,Routes,Route } from 'react-router-dom'
+import SignUp from './pages/SignUp.jsx'
+import SignIn from './pages/SignIn.jsx'
+import Dashboard from './pages/Dashboard.jsx'
+import Home from './pages/Home.jsx'
+import Header from './components/Header.jsx'
 export default function App() {
   return (
     <div >
-      APP
+      <BrowserRouter>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from "flowbite-react";
+
+
+export default function Header() {
+  return (
+    <div>
+      <Button color="blue">Blue</Button>
+      Header
+    </div>
+  )
+}
